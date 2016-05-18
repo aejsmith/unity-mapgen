c0c522f8969b597e1dffe1cec989a2ed053279ae
Alex Smith <alex@alex-smith.me.uk>
2016-05-18 12:04:32 +0100

    Add a static Instance reference to MapProperties
    This is set on Awake() to the MapProperties instance. This saves having
    to use GameObject methods to find it, which Unity recommends against for
    performance reasons.
    
    Signed-off-by: Alex Smith <alex@alex-smith.me.uk>

--- Assets/Map/Scripts/MapProperties.cs
+++ Assets/Map/Scripts/MapProperties.cs
@@ -11,5 +11,22 @@ namespace MapGen {
         public float TileSize;
         public Vector2 WorldSize;
         public Vector2 DetailedWorldSize;
+
+        /** Global instance of the class. */
+        public static MapProperties Instance { get { return m_instance; } }
+        private static MapProperties m_instance;
+
+        void Awake() {
+            /*
+             * Store a global pointer to this instance rather than using
+             * GameObject methods to look it up whenever we need it, as Unity
+             * docs recommend against doing this for performance reasons.
+             */
+            m_instance = this;
+        }
+
+        void OnApplicationQuit() {
+            m_instance = null;
+        }
     }
 }
